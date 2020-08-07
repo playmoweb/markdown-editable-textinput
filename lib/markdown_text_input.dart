18e6da5622fa72896c89d7cd95d4f90115624b16
clémence <clemence@playmoweb.com>
2020-08-07 15:52:23 +0200

    add support for web input

--- lib/markdown_text_input.dart
+++ lib/markdown_text_input.dart
@@ -20,22 +20,28 @@ class MarkdownTextInput extends StatefulWidget {
 
 class _MarkdownTextInputState extends State<MarkdownTextInput> {
   final _controller = TextEditingController();
+  TextSelection textSelection =
+      const TextSelection(baseOffset: 0, extentOffset: 0);
 
   void onTap(MarkdownType type, {int titleSize = 1}) {
-    final basePosition = _controller.selection.baseOffset;
+    final basePosition = textSelection.baseOffset;
 
-    final result = FormatMarkdown.convertToMarkdown(
-        type, _controller.text, _controller.selection.baseOffset, _controller.selection.extentOffset,
+    final result = FormatMarkdown.convertToMarkdown(type, _controller.text,
+        textSelection.baseOffset, textSelection.extentOffset,
         titleSize: titleSize);
 
-    _controller.value = _controller.value
-        .copyWith(text: result.data, selection: TextSelection.collapsed(offset: basePosition + result.cursorIndex));
+    _controller.value = _controller.value.copyWith(
+        text: result.data,
+        selection:
+            TextSelection.collapsed(offset: basePosition + result.cursorIndex));
   }
 
   @override
   void initState() {
     _controller.text = widget.initialValue;
     _controller.addListener(() {
+      if (_controller.selection.baseOffset != -1)
+        textSelection = _controller.selection;
       widget.onTextChanged(_controller.text);
     });
     super.initState();
@@ -62,26 +68,35 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
             maxLines: null,
             controller: _controller,
             textCapitalization: TextCapitalization.sentences,
-            validator: widget.validators != null ? (value) => widget.validators(value) as String : null,
+            validator: widget.validators != null
+                ? (value) => widget.validators(value) as String
+                : null,
             cursorColor: Theme.of(context).primaryColor,
             decoration: InputDecoration(
-              enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).accentColor)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).accentColor)),
               hintText: widget.label,
-              hintStyle: const TextStyle(color: Color.fromRGBO(63, 61, 86, 0.5)),
-              contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              hintStyle:
+                  const TextStyle(color: Color.fromRGBO(63, 61, 86, 0.5)),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
             ),
           ),
           Material(
             color: Theme.of(context).cardColor,
-            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+            borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
             child: Row(
               children: [
                 InkWell(
-                  key: const Key('bold_button'),
-                  onTap: () => onTap(MarkdownType.bold),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+                  onTap: () {
+                    onTap(MarkdownType.bold);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
                     child: Icon(
                       Icons.format_bold,
                     ),
@@ -90,8 +105,8 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                 InkWell(
                   key: const Key('italic_button'),
                   onTap: () => onTap(MarkdownType.italic),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
                     child: Icon(
                       Icons.format_italic,
                     ),
@@ -105,15 +120,17 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                       padding: const EdgeInsets.all(10),
                       child: Text(
                         'H$i',
-                        style: TextStyle(fontSize: (18 - i).toDouble(), fontWeight: FontWeight.w700),
+                        style: TextStyle(
+                            fontSize: (18 - i).toDouble(),
+                            fontWeight: FontWeight.w700),
                       ),
                     ),
                   ),
                 InkWell(
                   key: const Key('link_button'),
                   onTap: () => onTap(MarkdownType.link),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
                     child: Icon(
                       Icons.link,
                     ),
@@ -122,8 +139,8 @@ class _MarkdownTextInputState extends State<MarkdownTextInput> {
                 InkWell(
                   key: const Key('list_button'),
                   onTap: () => onTap(MarkdownType.list),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
                     child: Icon(
                       Icons.list,
                     ),
